c6d1bf78485494d0047999facb4aa81b3ead74d0
KChoiSlattery <kchoislattery@gatech.edu>
2025-07-27 04:04:03 -0400

    Add fonts, remove desmos mathquill

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -35,17 +35,17 @@ console.log("Running main.ts...")
 // }
 
 function load_example(mathfield: any) {
-    mathfield.latex("v^2=\\mu \\left(\\frac{2}{r}-\\frac{1}{a}\\right)");
+  mathfield.latex("v^2=\\mu \\left(\\frac{2}{r}-\\frac{1}{a}\\right)");
 }
 
 function fake_solve(output_span: HTMLSpanElement, copy_output_button: HTMLElement) {
-    katex.render("r=\\frac{2a\\mu }{av^2+\\mu }", output_span, {output: 'mathml'});
-    output_span.classList.remove('waiting');
-    copy_output_button.classList.remove('hidden');
+  katex.render("r=\\frac{2a\\mu }{av^2+\\mu }", output_span, { output: 'mathml' });
+  output_span.classList.remove('waiting');
+  copy_output_button.classList.remove('hidden');
 
 }
 
-function format_math_select (tex) {
+function format_math_select(tex) {
   if (!tex.id) {
     return tex.text;
   }
@@ -57,36 +57,35 @@ function format_math_select (tex) {
 };
 
 $(document).ready(function () {
-    const body = document.body;
-    const mathfield_span = document.getElementById("mathfield_span");
-    const example_button = document.getElementById("example_button");
-    const solve_button = document.getElementById("solve_button");
-    const output_span = document.getElementById("output_span");
-    const copy_output_button = document.getElementById("copy_output_button");
+  const body = document.body;
+  const mathfield_span = document.getElementById("mathfield_span");
+  const example_button = document.getElementById("example_button");
+  const solve_button = document.getElementById("solve_button");
+  const output_span = document.getElementById("output_span");
+  const copy_output_button = document.getElementById("copy_output_button");
 
-    
-    MathQuillLoader.loadMathQuill({ mode: 'dev' }, mathquill => {
-        var MQ = mathquill.getInterface(2);
-        var mathfield = MQ.MathField(mathfield_span, {
-            spaceBehavesLikeTab: false, // configurable
-        });
-
-        example_button.addEventListener("click", function() {
-            load_example(mathfield);
-        });
-
-        solve_button.addEventListener("click", function() {
-            fake_solve(output_span, copy_output_button);
-        });
-    });
-    
-    $('.math-select').select2({
-        templateResult: format_math_select,
-        templateSelection: format_math_select,
-        minimumResultsForSearch: 20
+  MathQuillLoader.loadMathQuill({ mode: 'dev' }, mathquill => {
+    var MQ = mathquill.getInterface(2);
+    var mathfield = MQ.MathField(mathfield_span, {
+      spaceBehavesLikeTab: false, // configurable
     });
 
-    body.classList.remove('loading'); // Remove the hiding class
+    example_button.addEventListener("click", function () {
+      load_example(mathfield);
+    });
+
+    solve_button.addEventListener("click", function () {
+      fake_solve(output_span, copy_output_button);
+    });
+  });
+
+  $('.math-select').select2({
+    templateResult: format_math_select,
+    templateSelection: format_math_select,
+    minimumResultsForSearch: 20
+  });
+
+  body.classList.remove('loading'); // Remove the hiding class
 });
 
 console.log("Successfully ran main.ts!")
